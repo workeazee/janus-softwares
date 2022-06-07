a130a03b7d323ae1072e9dbcfc8d1a1d3936130a
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-06-07 17:24:07 +0530

    add: main page structure

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import ServicesPage from "./pages/ServicesPage";
 import TechPage from "./pages/TechPage";
-
+import AboutPage from "./pages/AboutPage";
+import Footer from "./pages/Footer";
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{"overflow-x": "hidden"}}>
       <Header />
       <MainPage />
       <ServicesPage />
       <TechPage />
+      <AboutPage />
+      <Footer />
     </div>
   );
 }
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const MainDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d2d2d2;
+  height: 50vh;
+  width: 100vw;
+  padding: 0 1.5rem;
+`;
 const Footer = () => {
-    return <div></div>;
+    return <MainDiv></MainDiv>;
 }
 
 export default Footer;
