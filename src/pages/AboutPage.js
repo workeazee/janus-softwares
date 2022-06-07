622c7c6d4ded57232efe38124f198909c7b6792d
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-06-08 00:10:40 +0530

    add: pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,17 +1,152 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Images from "../assets/images.jpg";
+import { FiRefreshCw } from "react-icons/fi";
+import { BsFillFileEarmarkArrowDownFill } from "react-icons/bs";
+import { BiTime } from "react-icons/bi";
+import { CgSearchFound } from "react-icons/cg";
 
 const MainDiv = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
   justify-content: center;
-  background-color: #d1b3d1;
   height: 100vh;
   width: 100vw;
-  padding: 0 1.5rem;
+  padding: 4rem 7rem;
+`;
+const SmallHeader = styled.div`
+  font-weight: 500;
+  font-size: 1.3rem;
+`;
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 90%;
+  width: 100%;
+`;
+const ContentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 40%;
+  height: 100%;
+  font-size: 3rem;
+  line-height: 1;
+  padding-top: 1.5rem;
+  position: relative;
+`;
+const ImageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 60%;
+  height: 100%;
+  position: relative;
+  margin-left: 2rem;
+`;
+const Image = styled.img`
+  width: 80%;
+  height: 60%;
+  object-fit: cover;
+  border-radius: 1rem;
+  margin-top: 2rem;
+`;
+const Paragraph = styled.div``;
+const Box1 = styled.div`
+  height: 20vh;
+  width: 15vw;
+  background-color: yellow;
+  z-index: 1199;
+  position: absolute;
+  bottom: 0;
+  left: -2rem;
+  border-radius: 1rem;
+  animation-name: floating;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+
+  @keyframes float {
+    0% {
+      transform: translate(0, 0px);
+    }
+    50% {
+      transform: translate(25px, 0);
+    }
+    100% {
+      transform: translate(0, -0px);
+    }
+  }
+`;
+const Tile = styled.div`
+  height: 90%;
+  width: 40%;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+const Heading = styled.div`
+  font-size: 2rem;
+`;
+const ParagraphSmall = styled.div`
+  color: GREY;
+  font-size: 1.1rem;
 `;
 const AboutPage = () => {
-    return <MainDiv></MainDiv>;
-}
+  return (
+    <MainDiv>
+      <SmallHeader>Why Choose Us?</SmallHeader>
+      <Flex>
+        <ContentDiv>
+          <Paragraph>Make Work Productive & Get Faster Solution</Paragraph>
+          <Image src={Images} alt="images" />
+          <Box1 />
+        </ContentDiv>
+        <ImageDiv>
+          <Flex>
+            <Tile>
+              <FiRefreshCw size={48} />
+              <Heading>Advanced Analysis</Heading>
+              <ParagraphSmall>
+                We handle things by first analyzing the problem and then taking
+                the best approach to reach the solution.
+              </ParagraphSmall>
+            </Tile>
+            <Tile>
+              <BsFillFileEarmarkArrowDownFill size={48} />
+              <Heading>Optimisation</Heading>
+              <ParagraphSmall>
+                Every line of code we write is properly optimized so our clients
+                may never face any unwanted glitches.
+              </ParagraphSmall>
+            </Tile>
+          </Flex>
+          <Flex>
+            <Tile>
+              <BiTime size={48} />
+              <Heading>On Time</Heading>
+              <ParagraphSmall>
+                Things may seem a little swingy sometimes but our promise to
+                deliver does not waver a speck.
+              </ParagraphSmall>
+            </Tile>
+            <Tile>
+              <CgSearchFound size={48} />
+              <Heading>Continous Testing</Heading>
+              <ParagraphSmall>
+                Work we do here is tested continuously so that the chances of
+                clingy bugs is reduced to null.
+              </ParagraphSmall>
+            </Tile>
+          </Flex>
+        </ImageDiv>
+      </Flex>
+    </MainDiv>
+  );
+};
 
 export default AboutPage;
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const MainDiv = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   background-color: #d2d2d2;
   height: 50vh;
   width: 100vw;
   padding: 0 1.5rem;
+  position: relative;
+  margin: 0 auto;
 `;
+const NewsLetterModal = styled.div`
+  height: 20vh;
+  width: 75%;
+  background-color: yellow;
+  z-index: 9999;
+  transform: translateY(-10vh);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: row;
+`;
+const Subscribe = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+const Email = styled.div`
+  height: 100%;
+  width: 60%;
+  // background-color: blue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const MainHeading = styled.h3``;
+const SmallParagraph = styled.p``;
+const Input = styled.input`
+  background-color: white;
+  border-radius: 1rem;
+  border: 1px solid black;
+  height: 3rem;
+`;
+const Button = styled.button``;
 const Footer = () => {
-    return <MainDiv></MainDiv>;
-}
+  return (
+    <MainDiv>
+      <NewsLetterModal>
+        <Subscribe>
+          <MainHeading>Subscribe Our Newsletter</MainHeading>
+          <SmallParagraph>
+            Get updates on the amazing work we do at constbase and to get
+            notified on the discounted services that we offer
+          </SmallParagraph>
+        </Subscribe>
+        <Email>
+          <Input type="text" placeholder="Enter your email" />
+          <Button>Subscribe</Button>
+        </Email>
+      </NewsLetterModal>
+    </MainDiv>
+  );
+};
 
 export default Footer;
